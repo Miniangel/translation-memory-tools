96917a624d8ee69f556aa3b3a54bb7034367c536
Jordi Mas <jmas@softcatala.org>
2020-05-23 22:42:25 +0200

    Fixes

--- docker/entry-point-builder.sh
+++ docker/entry-point-builder.sh
@@ -8,6 +8,11 @@ PREPROD_DEPLOY_DIR=/public-data/web/recursos-preprod
 echo $DIR, $DIR_TMT_GIT
 cd $DIR_TMT_GIT && git pull
 
+mkdir -p ~/.ssh && chmod 0700 ~/.ssh
+cp /run/secrets/ssh_private_key  ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
+eval `ssh-agent -s` && ssh-add -k ~/.ssh/id_rsa
+ssh-keyscan -p 3222 -H pirineus.softcatala.org >> ~/.ssh/known_hosts 
+
 if [[ -n "${TRANSIFEX_USER}" && -n "${TRANSIFEX_PASSWORD}" ]]; then
     python $DIR_TMT_GIT/docker/credentials/transifex.py
 else
@@ -25,16 +30,22 @@ fi
 tx --version
 
 cd $DIR_TMT_GIT/deployment 
-/bin/bash generate-tm.sh $DIR $PUBLIC
+/bin/bash generate-tm.sh $DIR $PUBLIC 2> $DIR_TMT_GIT/generate-errors.log
 if [ -f builder-error.log ]; then cat builder-error.log;fi
 
-/bin/bash generate-terminology.sh $DIR
-/bin/bash generate-isolists.sh $DIR
+/bin/bash generate-terminology.sh $DIR 2> $DIR_TMT_GIT/terminology-errors.log
+/bin/bash generate-isolists.sh $DIR 2> $DIR_TMT_GIT/iso-lists-errors.log
 
 cd $DIR_TMT_GIT/deployment
-/bin/bash generate-quality.sh $DIR
+/bin/bash generate-quality.sh $DIR 2> $DIR_TMT_GIT/quality-errors.log
 /bin/bash deploy.sh $DIR $DEPLOY_DIR "" $PUBLIC
 
-bash
+# Deploy
+rsync -avz --delete -e "ssh -p 3222" $DEPLOY_DIR/indexdir/ tmt-files@pirineus.softcatala.org:/home/jmas/web/recursos-dev/indexdir/
+rsync -avz --delete -e "ssh -p 3222" $DEPLOY_DIR/*.html tmt-files@pirineus.softcatala.org:/home/jmas/web/recursos-dev/
+rsync -avz --delete -e "ssh -p 3222" $DEPLOY_DIR/quality/ tmt-files@pirineus.softcatala.org:/home/jmas/web/recursos-dev/quality/
+rsync -avz --delete -e "ssh -p 3222" $DEPLOY_DIR/memories/ tmt-files@pirineus.softcatala.org:/home/jmas/web/recursos-dev/memories/
+
+echo "Waiting for 2 days"
 sleep 2d
 
