89ba7d36683a346d12476b4f209c156b6570397c
Jordi Mas <jmas@softcatala.org>
2020-05-25 09:13:21 +0200

    Fix

--- docker/entry-point-builder.sh
+++ docker/entry-point-builder.sh
@@ -26,12 +26,11 @@ fi
 
 tx --version
 
+# Build
 cd $DIR_TMT_GIT/deployment 
 /bin/bash generate-tm.sh $DIR $PUBLIC 2> $DIR_TMT_GIT/generate-errors.log
 /bin/bash generate-terminology.sh $DIR 2> $DIR_TMT_GIT/terminology-errors.log
 /bin/bash generate-isolists.sh $DIR 2> $DIR_TMT_GIT/iso-lists-errors.log
-
-cd $DIR_TMT_GIT/deployment
 /bin/bash generate-quality.sh $DIR 2> $DIR_TMT_GIT/quality-errors.log
 /bin/bash deploy.sh $DIR $DEPLOY_DIR "" $PUBLIC
 
@@ -41,6 +40,7 @@ rsync -avz --delete -e "ssh -p 3222" $DEPLOY_DIR/*.html tmt-files@pirineus.softc
 rsync -avz --delete -e "ssh -p 3222" $DEPLOY_DIR/quality/ tmt-files@pirineus.softcatala.org:/home/jmas/web/recursos-dev/quality/
 rsync -avz --delete -e "ssh -p 3222" $DEPLOY_DIR/memories/ tmt-files@pirineus.softcatala.org:/home/jmas/web/recursos-dev/memories/
 
+# Report
 cd $DIR_TMT_GIT/docker
 /bin/bash generate-email.sh $DIR $DEPLOY_DIR $SECONDS
 
