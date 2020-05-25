2200fcdd4d7bcc80c4d9244c03d2d977ffccbaf8
Jordi Mas <jmas@softcatala.org>
2020-05-25 09:21:31 +0200

    Fix

--- docker/generate-email.sh
+++ docker/generate-email.sh
@@ -9,7 +9,6 @@ BACKUP_DIR=$PUBLIC/previous
 cd $ROOT/tm-git/src
 python compare_sets.py -s  $BACKUP_DIR -t $INTERMEDIATE_PO > report.txt
 ls -h -s -S  $TARGET_DIR/quality/*.html >> report.txt
-cd $ROOT/tm-git/src
 cat builder-error.log >> report.txt
-
-printf 'TIME. Total execution time %dh:%dm:%ds\n' $(($secs/3600)) $(($secs%3600/60)) $(($secs%60))
+printf 'TIME. Total execution time %dh:%dm:%ds\n' $(($secs/3600)) $(($secs%3600/60)) $(($secs%60)) >> report.txt
+python ../docker/send-email.py
