37bdd465dafca27cd6ce01fd24de51ceb57d78e4
Jordi Mas <jmas@softcatala.org>
2020-05-31 13:56:09 +0200

    Fixes

--- docker/entry-point-builder.sh
+++ docker/entry-point-builder.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 DIR="$1"
 DIR_TMT_GIT="$2"
-PUBLIC=/public-data/
-DEPLOY_DIR=/public-data/web/recursos-dev
-PREPROD_DEPLOY_DIR=/public-data/web/recursos-preprod
+PUBLIC=/srv/public-data/
+DEPLOY_DIR=/srv/web/recursos-dev
+PREPROD_DEPLOY_DIR=/web/recursos-preprod
 
 mkdir -p ~/.ssh && chmod 0700 ~/.ssh
 cp /run/secrets/ssh_private_key  ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
 eval `ssh-agent -s` && ssh-add -k ~/.ssh/id_rsa
-ssh-keyscan -p 3222 -H pirineus.softcatala.org >> ~/.ssh/known_hosts 
+ssh-keyscan -p 3333 -H gitlab.softcatala.org >> ~/.ssh/known_hosts 
+git config --global user.email "jmas@softcatala.org"
+git config --global user.name "TMT builder"
+
+
+cd /srv
+git clone ssh://git@gitlab.softcatala.org:3333/github/translation-memory-tools-files.git public_data
 
 if [[ -n "${TRANSIFEX_USER}" && -n "${TRANSIFEX_PASSWORD}" ]]; then
     python $DIR_TMT_GIT/docker/credentials/transifex.py
@@ -35,16 +41,20 @@ cd $DIR_TMT_GIT/deployment
 /bin/bash deploy.sh $DIR $DEPLOY_DIR "" $PUBLIC
 
 # Deploy
-rsync -avz --delete -e "ssh -p 3222" $DEPLOY_DIR/indexdir/ tmt-files@pirineus.softcatala.org:/home/jmas/web/recursos-dev/indexdir/
-rsync -avz --delete -e "ssh -p 3222" $DEPLOY_DIR/*.html tmt-files@pirineus.softcatala.org:/home/jmas/web/recursos-dev/
-rsync -avz --delete -e "ssh -p 3222" $PUBLIC/*.db3 tmt-files@pirineus.softcatala.org:/home/jmas/web/recursos-dev/
-rsync -avz --delete -e "ssh -p 3222" $DEPLOY_DIR/quality/ tmt-files@pirineus.softcatala.org:/home/jmas/web/recursos-dev/quality/
-rsync -avz --delete -e "ssh -p 3222" $DEPLOY_DIR/memories/ tmt-files@pirineus.softcatala.org:/home/jmas/web/recursos-dev/memories/
+cd $PUBLIC
+cp -r $DEPLOY_DIR/indexdir .
+cp -r $DEPLOY_DIR/*.html .
+git add *
+git commit -a -m "Changes"
+git push
+bash
+#rsync -avz --delete -e "ssh -p 3222" $DEPLOY_DIR/indexdir/ tmt-files@pirineus.softcatala.org:/home/jmas/web/recursos-dev/indexdir/
+#rsync -avz --delete -e "ssh -p 3222" $DEPLOY_DIR/*.html tmt-files@pirineus.softcatala.org:/home/jmas/web/recursos-dev/
+#rsync -avz --delete -e "ssh -p 3222" $PUBLIC/*.db3 tmt-files@pirineus.softcatala.org:/home/jmas/web/recursos-dev/
+#rsync -avz --delete -e "ssh -p 3222" $DEPLOY_DIR/quality/ tmt-files@pirineus.softcatala.org:/home/jmas/web/recursos-dev/quality/
+#rsync -avz --delete -e "ssh -p 3222" $DEPLOY_DIR/memories/ tmt-files@pirineus.softcatala.org:/home/jmas/web/recursos-dev/memories/
 
 # Report
-cd $DIR_TMT_GIT/docker
-/bin/bash generate-email.sh $DIR $DEPLOY_DIR $SECONDS
-
-echo "Waiting for 2 days"
-sleep 2d
+#cd $DIR_TMT_GIT/docker
+#/bin/bash generate-email.sh $DIR $DEPLOY_DIR $SECONDS
 
