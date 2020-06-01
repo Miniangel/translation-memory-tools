bb47f6a00d10bfef2f82644d3e0bf17fa366506a
Jordi Mas <jmas@softcatala.org>
2020-06-01 19:30:17 +0200

    Fix

--- docker/entry-point-builder.sh
+++ docker/entry-point-builder.sh
@@ -5,14 +5,13 @@ PUBLIC=/srv/public-data
 #DEPLOY_DIR=/srv/web/recursos-dev
 #PREPROD_DEPLOY_DIR=/web/recursos-preprod
 
-echo $PRIVATE_KEY
+echo "Key: $PRIVATE_KEY"
 mkdir -p ~/.ssh && chmod 0700 ~/.ssh
 echo "$PRIVATE_KEY" > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
 eval `ssh-agent -s` && ssh-add -k ~/.ssh/id_rsa
 ssh-keyscan -p 3333 -H gitlab.softcatala.org >> ~/.ssh/known_hosts 
 git config --global user.email "jmas@softcatala.org"
 git config --global user.name "TMT builder"
-
 
 cd /srv
 git clone ssh://git@gitlab.softcatala.org:3333/github/translation-memory-tools-files.git public-data
@@ -28,9 +27,9 @@ fi
 if [[ -n "${ZANATA_PROJECT_1}" && -n "${ZANATA_USER_1}"  && -n "${ZANATA_TOKEN_1}" ]]; then
     python $DIR_TMT_GIT/docker/credentials/zanata.py $DIR_TMT_GIT/cfg/credentials/
 else
-    echo "Removing Zentata projects"
+    echo "Removing Zenata projects"
     grep -l "type.*zanata" cfg/projects/*.json  | xargs rm -f
-
+fi
 
 # Build
 cd $DIR_TMT_GIT/deployment 
