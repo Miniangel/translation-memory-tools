aa49a1f9aad4b98a88ca7c6f5b67b056c618cdcb
Jordi Mas <jmas@softcatala.org>
2020-09-09 10:57:58 +0200

    Remove nginx

--- docker/entry-point-web.sh
+++ docker/entry-point-web.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# https://www.softcatala.org/recursos/ es desvia al port 8080 de la màquina virtual interna recursos on hi ha un nginx contestant el port i el #gunicorn contestant al port 8000
 #http://172.17.0.2:8080/
-/etc/init.d/nginx start
 cd /srv/web
-gunicorn web_search:app -b 0.0.0.0:8000
+gunicorn web_search:app -b 0.0.0.0:8080
 
